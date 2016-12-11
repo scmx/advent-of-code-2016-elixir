79560d5bfc841537c6dc9c0c44ca0a4d6e53fd7f
Albert Arvidsson <albert.arvidsson@gmail.com>
2016-12-11 16:25:08 +0100

    Add Day05EightCharPass.better_eight_char_pass/1

--- lib/day_05_eight_char_pass.ex
+++ lib/day_05_eight_char_pass.ex
@@ -23,4 +23,48 @@ defmodule Adventofcode.Day05EightCharPass do
 
   defp five_zeroes?(%{hash: "00000" <> _}), do: true
   defp five_zeroes?(%{hash: _}), do: false
+
+  def better_eight_char_pass(door_id) do
+    door_id
+    |> better_eight_char_pass_stream
+    |> Enum.reduce_while(%{}, &password_reducer/2)
+  end
+
+  def better_eight_char_pass_stream(door_id) do
+    %Row{door_id: door_id}
+    |> Stream.iterate(&increasing_index/1)
+    |> Stream.map(&assign_hash/1)
+    |> Stream.filter(&five_zeroes_and_pos?/1)
+    |> Stream.map(&assign_position_and_letter/1)
+  end
+
+  defp five_zeroes_and_pos?(%{hash: "000000" <> _}), do: true
+  defp five_zeroes_and_pos?(%{hash: "000001" <> _}), do: true
+  defp five_zeroes_and_pos?(%{hash: "000002" <> _}), do: true
+  defp five_zeroes_and_pos?(%{hash: "000003" <> _}), do: true
+  defp five_zeroes_and_pos?(%{hash: "000004" <> _}), do: true
+  defp five_zeroes_and_pos?(%{hash: "000005" <> _}), do: true
+  defp five_zeroes_and_pos?(%{hash: "000006" <> _}), do: true
+  defp five_zeroes_and_pos?(%{hash: "000007" <> _}), do: true
+  defp five_zeroes_and_pos?(%{hash: _}), do: false
+
+  defp assign_position_and_letter(row) do
+    position = row.hash |> String.at(5) |> String.to_integer
+    letter = row.hash |> String.at(6)
+    %{row | position: position, letter: letter}
+  end
+
+  defp password_reducer(line, acc) do
+    cond do
+      acc |> password_complete? -> {:halt, acc |> password_combine}
+      acc |> Map.has_key?(line.position) -> {:cont, acc}
+      true -> {:cont, acc |> Map.put(line.position, line.letter)}
+    end
+  end
+
+  defp password_complete?(lines),
+    do: 0..7 |> Enum.all?(&Map.has_key?(lines, &1))
+
+  defp password_combine(lines),
+    do: 0..7 |> Enum.reduce("", &"#{&2}#{lines |> Map.get(&1)}")
 end
--- test/day_05_eight_char_pass_test.exs
+++ test/day_05_eight_char_pass_test.exs
@@ -42,4 +42,32 @@ defmodule Adventofcode.Day05EightCharPassTest do
       assert "00000155f8105dff7f56ee10fa9b9abd" = "abc3231929" |> md5
     end
   end
+
+  test "better_eight_char_pass_stream/1" do
+    stream = "abc" |> better_eight_char_pass_stream
+    assert [
+      %Row{
+        door_id: "abc", index: 3231929, position: 1, letter: "5",
+        hash: "00000155f8105dff7f56ee10fa9b9abd"
+      },
+      %Row{
+        door_id: "abc", index: 5357525, position: 4, letter: "e",
+        hash: "000004e597bd77c5cd2133e9d885fe7e"
+      },
+    ] == stream |> Enum.take(2)
+  end
+
+  describe "better_eight_char_pass/1" do
+    @tag timeout: 240000
+    test "with example input" do
+      assert "05ace8e3" = "abc" |> better_eight_char_pass
+    end
+
+    @tag timeout: 240000
+    test "with puzzle input" do
+      with_puzzle_input "input/day_05_eight_char_pass.txt", fn input ->
+        assert "f2c730e5" = input |> better_eight_char_pass
+      end
+    end
+  end
 end
